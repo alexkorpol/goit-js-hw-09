11a9e41785f419a38167653d806ea9cec31c4b82
Alexander <alexkorpol1964@gmail.com>
2023-03-29 11:36:24 +0300

    Continue work Task#2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,29 +1,26 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-// const selectData = document.querySelector("#datetime-picker");
-// console.log("🚀 ~ file: 02-timer.js:4 ~ selectData:", selectData);
-const startBtn = document.querySelector('button[data-start]');
-startBtn.addEventListener("click", () => {
-  console.log(options.onClose(selectedDates));
-});
+const fildOfSelectData = document.querySelector("input#datetime-picker");
+const remainDays = document.querySelector('[data-days]');
+const remainHours = document.querySelector('[data-hours]');
+const remainMinutes = document.querySelector('[data-minutes]');
+const remainSecond = document.querySelector('[data-seconds]');
+
+document.querySelector('button[data-start]')
+  .addEventListener("click", () => { });
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log("==>>",selectedDates[0]);
-  },
-};
-console.log("🚀 before options:", options);
-// selectData.addEventListener('click', options);
-const selectedDates = flatpickr("input#datetime-picker", { options });
-// console.log("🚀 ~ file: 02-timer.js:17 ~ selectData:", selectData);
-console.log("🚀 after options:", options);
-console.log(options.defaultDate);
-console.log(options.defaultDate.getDate());
-console.log(options.defaultDate.getHours());
-console.log(options.defaultDate.getMinutes());
-console.log(options.defaultDate.getSeconds());
-console.log(options.onClose(selectedDates));
+  console.log("==>>", selectedDates[0]);
+  }
+}
+
+
+flatpickr(fildOfSelectData, { options });
+
+
 
