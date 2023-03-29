d1cb69fe2b6a24228fae904bf3b01845eecd10de
Alexander <alexkorpol1964@gmail.com>
2023-03-29 17:57:51 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,26 +1,36 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-const fildOfSelectData = document.querySelector("input#datetime-picker");
-const remainDays = document.querySelector('[data-days]');
-const remainHours = document.querySelector('[data-hours]');
-const remainMinutes = document.querySelector('[data-minutes]');
-const remainSecond = document.querySelector('[data-seconds]');
+const inputEl = document.querySelector("input#datetime-picker");
+// const remainDays = document.querySelector('[data-days]');
+// const remainHours = document.querySelector('[data-hours]');
+// const remainMinutes = document.querySelector('[data-minutes]');
+// const remainSecond = document.querySelector('[data-seconds]');
+// let targetDate = null;
 
-document.querySelector('button[data-start]')
-  .addEventListener("click", () => { });
+// document.querySelector('button[data-start]')
+//   .addEventListener("click", () => {
+//     targetDate = new Date(inputEl.value);
+//     console.log("targetDate:", targetDate);
+//   });
+
 
 const options = {
   enableTime: true,
   time_24hr: true,
-  defaultDate: new Date(),
+  defaultDate: Date.now(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-  console.log("==>>", selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    console.log(selectedDate);
+    console.log(options.defaultDate);
+
+    if(selectedDates[0] < options.defaultDate) {
+      return alert("Please choose a date in the future");
+    }
+    alert("Date is selected correct");
   }
 }
 
 
-flatpickr(fildOfSelectData, { options });
-
-
+flatpickr(inputEl, { options });
 
