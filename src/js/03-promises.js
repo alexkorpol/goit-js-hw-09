8330c58fc926638f677a303c0696311961ab47e3
Alexander <alexkorpol1964@gmail.com>
2023-04-03 16:59:07 +0300

    Continue Task#3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,39 +1,39 @@
-const arrInput = document.querySelectorAll('input');
-let delayInput = arrInput[0];
-let stepInput = arrInput[1];
-let amountInput = arrInput[2];
+import Notiflix from 'notiflix';
+const arr = document.querySelectorAll('input');
+// const form = document.querySelector('.form');
+
+let delay = arr[0];
+let step = arr[1];
+let amount = arr[2];
 const buttonCreate = document.querySelector('button');
+
 
 buttonCreate.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log("delayInput.value:", delayInput.value, "stepInput.value:", stepInput.value, "amountInput.value:",
-    amountInput.value);
-  for (let i = 0; i <= amountInput.value; i += 1){
-    const delayCount = Number(delayInput.value) + Number(stepInput.value) * i;
-    // console.log("i:", i, "delayCount", delayCount);
-    createPromise(i + 1, Number(delayInput.value));
+  for (let i = 0; i <= amount.value-1; i += 1){
+    const delayCount = Number(delay.value) + Number(step.value) * i;
+    createPromise(i + 1, delayCount)
+      .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+     .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
   }
-
 });
-
-// delayInput.value = 5000;
-// stepInput.value = 500;
-// amountInput.value = 5;
-// console.log("arrInput", arrInput);
-
-
-
 
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      console.log(`Fulfilled promise ${position} in ${delay} ms`);
-      // Fulfill
-    } else {
-      console.log(`Rejected promise ${position} in ${delay} ms`)
-      // Reject
-    }
-  }
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+        // Fulfill
+      } else {
+        reject({ position, delay });
+        // Reject
+      }
+    }, delay )
+  });
 }
